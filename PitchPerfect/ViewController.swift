eac081b7ecdfde7aaa3bb215c32f3a6fdab14508
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-11-18 06:56:36 -0300

    Add some outlets

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,14 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad")
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
     }
 
     @IBAction func recordAudio(_ sender: Any) {
         print("record button was pressed")
+        recordingLabel.text = "Recording in Progress"
     }
 
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop recording button was pressed")
+    }
 }
 
