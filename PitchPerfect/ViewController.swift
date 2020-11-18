d7cd96743cd1d6c6b86877ae32efbcdce51775de
Ataias Pereira Reis <ataiasreis@gmail.com>
2020-11-18 05:38:15 -0300

    Add initial commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Ataias Pereira Reis on 17/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed")
+    }
+
+}
+
